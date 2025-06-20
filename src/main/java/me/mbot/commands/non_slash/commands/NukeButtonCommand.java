e6b4b07ec34996953c1c2850b89181b3ccf6bb70
acexcy <79228180+acexcy@users.noreply.github.com>
2025-06-20 23:27:49 +0200

    add India to &Button command

--- src/main/java/me/mbot/commands/non_slash/commands/NukeButtonCommand.java
+++ src/main/java/me/mbot/commands/non_slash/commands/NukeButtonCommand.java
@@ -19,6 +19,7 @@ public class NukeButtonCommand implements NonSlashCommandHandler {
         countriesToBeNuked.put("France", "https://media.discordapp.net/attachments/1082718875038273566/1362900942630158376/Picsart_25-04-18_23-21-34-068.jpg?ex=6804bcaa&is=68036b2a&hm=3c6cc1e52b30e88052a4aa020f01974786a7347b924fa3d76ea457ae6f48cbeb&=&format=webp");
         countriesToBeNuked.put("Saudi Arabia", "https://media.discordapp.net/attachments/1082718875038273566/1363240262402572359/Picsart_25-04-19_21-49-41-562.jpg?ex=68054fee&is=6803fe6e&hm=e0d90fefa49e691de9357ea5c568cb59c7248f743c157f19c5d4ac1f61a7fa99&=&format=webp");
         countriesToBeNuked.put("Russia", "https://media.discordapp.net/attachments/1082718875038273566/1365270384588619867/Picsart_25-04-25_12-16-49-945.jpg?ex=680cb2a1&is=680b6121&hm=008b04891aa352a73bc5ac17bddd7b8ddc24e02e945088e621e87352973607a2&=&format=webp&width=648&height=648");
+        countriesToBeNuked.put("India", "https://media.discordapp.net/attachments/704321731325853706/1376566336146243714/India.jpg?ex=6856c04d&is=68556ecd&hm=cae2b75422138a7d1636db0c8cdb7ab61f830623341e2c871226d29fb6bf2ce5&=&format=webp&width=743&height=743");
     }
 
     @Override
