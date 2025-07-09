cbfeb3c3df155b51931ce74449f27124334a37ec
acexcy <79228180+acexcy@users.noreply.github.com>
2025-07-09 13:20:52 +0200

    unnecessary

--- src/main/java/me/mbot/configuration/api/CommandMetaData.java
+++ src/main/java/me/mbot/configuration/api/CommandMetaData.java
@@ -5,17 +5,8 @@ package me.mbot.configuration.api;
  * This interface will probably not change a lot, make sure to tell all developers if it does as it might break a LOT of commands.
  */
 public interface CommandMetaData {
-
-    /**
-     *
-     * @return Name of the command as a String.
-     */
+    
     String getName();
-
-    /**
-     *
-     * @return Description of the command as a String.
-     */
     String getDescription();
 
 }
