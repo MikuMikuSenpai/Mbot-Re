839745830b6cd52f074602f57b3fdc033764d581
acexcy <79228180+acexcy@users.noreply.github.com>
2025-08-07 01:24:50 +0200

    remove unused imports

--- src/main/java/me/mbot/configuration/Constants.java
+++ src/main/java/me/mbot/configuration/Constants.java
@@ -1,12 +1,6 @@
 package me.mbot.configuration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.Properties;
 
 public class Constants {
 
